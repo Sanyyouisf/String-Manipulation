2d31a267596929229f9bdbdd5f111e48abfba584
Sanyyouisf <sanymamdoh@yahoo.com>
2017-02-25 03:17:48 -0600

    add two func

---
+++ strings.js
@@ -0,0 +1,39 @@
+
+
+
+var inputString = document.getElementById("inputString");
+var outputString = document.getElementById("outputString");
+var btnString = document.getElementById("btnString");
+
+// function to reverse the string
+function reversal(string) {
+	outputString.innerHTML= '<p>'+" the reverses order of the characters are :   "
+								 + string.split('').reverse().join('')+'</p>';
+}
+
+//function to sort the string
+// and add it to the Dom with new line 
+function alphabits(string) {
+	outputString.innerHTML+= '<p>'+" the characters in alphabetical order are :   "
+							      + string.split('').sort().join('') +'</p>';
+}
+
+
+function palindrome() {
+	
+}
+
+
+// event listener for the button click 
+btnString.addEventListener("click", function() {
+	reversal(inputString.value);
+	alphabits(inputString.value);
+});
+
+// event for clicking the enter button in kb 
+inputString.addEventListener("keypress",
+		function(event){
+			if (event.keyCode === 13){
+				reversal(inputString.value);
+				alphabits(inputString.value);}
+			});
